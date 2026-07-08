1043299740eb9522ba678c9b69d2612bb3b1e99a
Dmitry Shishkin <dmi3.shishkin@gmail.com>
2026-07-08 14:12:20 +0800

    devgate 0.8.0

--- Formula/devgate.rb
+++ Formula/devgate.rb
@@ -1,28 +1,28 @@
 class Devgate < Formula
   desc "Put localhost on the internet — secure tunnels to your local ports"
   homepage "https://devgate.online"
-  version "0.7.0"
+  version "0.8.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://devgate.online/downloads/agent/v0.7.0/devgate-0.7.0-darwin-arm64.tar.gz"
-      sha256 "e09eff26ee3281febad8b950f190a2e96e47a21803697068b890afd45211c16c"
+      url "https://devgate.online/downloads/agent/v0.8.0/devgate-0.8.0-darwin-arm64.tar.gz"
+      sha256 "4f71f41f1f3f084408fc7262d675a1cbe38dfa359ca25a239b8fcf68f095d445"
     end
     on_intel do
-      url "https://devgate.online/downloads/agent/v0.7.0/devgate-0.7.0-darwin-amd64.tar.gz"
-      sha256 "db4ec78cbac5f1039fc0aa2c2f51e0bd61387c0c72886ed2b002e6034366d894"
+      url "https://devgate.online/downloads/agent/v0.8.0/devgate-0.8.0-darwin-amd64.tar.gz"
+      sha256 "6f6751c72c8efdf5c5cee2878e292738c86345c3a747a5d30a68618cce2e74a6"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://devgate.online/downloads/agent/v0.7.0/devgate-0.7.0-linux-arm64.tar.gz"
-      sha256 "a97bf4db925fd49bcdf27c308d07fdaefd922bb873f5fbf9f129a222d5216b2d"
+      url "https://devgate.online/downloads/agent/v0.8.0/devgate-0.8.0-linux-arm64.tar.gz"
+      sha256 "e6dda743f7d2962b9024f7fd4653cb8a652d4ecb7db162736aba76e715d4cb1e"
     end
     on_intel do
-      url "https://devgate.online/downloads/agent/v0.7.0/devgate-0.7.0-linux-amd64.tar.gz"
-      sha256 "f31afab4e7ceb24d330eb4dec2655d0a579e732950f015a94f80a74f0c12d0c1"
+      url "https://devgate.online/downloads/agent/v0.8.0/devgate-0.8.0-linux-amd64.tar.gz"
+      sha256 "9f8a334107da8b033b67ca473cbf9c3e277f4f8c0e54568407af1b191564950d"
     end
   end
 
@@ -31,6 +31,6 @@ class Devgate < Formula
   end
 
   test do
-    assert_match "devgate 0.7.0", shell_output("#{bin}/devgate --version")
+    assert_match "devgate 0.8.0", shell_output("#{bin}/devgate --version")
   end
 end
